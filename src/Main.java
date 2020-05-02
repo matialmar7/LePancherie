e5e03ee1e9672913677eac004da4db4a64fdd721
matialmar7 <matiasalmarcha@hotmail.com>
2020-05-02 18:03:28 -0300

    Commit Inicial

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello World!");
-        System.out.println("Este es un commit desde IntelliJ");
+
     }
 }
