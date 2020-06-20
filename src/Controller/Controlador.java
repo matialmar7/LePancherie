31cf22011cd9a9790215fd681efa2f4a64584f33
matialmar7 <matiasalmarcha@hotmail.com>
2020-06-19 21:57:22 -0300

    Controlador y modelo separado de la vista

--- src/Controller/Controlador.java
+++ src/Controller/Controlador.java
@@ -38,7 +38,38 @@ public class Controlador {
     //METODOS CURSORES
 
     public void Cursor_onClick(ActionEvent event){
-        src.Main.modelo.comprarMejora(Model.Mejoras.CURSORES);
+        modelo.comprarMejora(Model.Mejoras.CURSORES);
+    }
+    public void Condimentos_onClick(ActionEvent event){
+        modelo.comprarMejora(Model.Mejoras.CONDIMENTOS);
+    }
+    public void Papas_onClick(ActionEvent event){
+        modelo.comprarMejora(Model.Mejoras.PAPAS);
+    }
+    public void Bebidas_onClick(ActionEvent event){
+        modelo.comprarMejora(Model.Mejoras.BEBIDAS);
+    }
+    public void Parrillas_onClick(ActionEvent event){
+        modelo.comprarMejora(Model.Mejoras.PARRILLAS);
+    }
+    public void Salchichera_onClick(ActionEvent event){
+        modelo.comprarMejora(Model.Mejoras.SALCHICHERA);
+    }
+    public void Heladeras_onClick(ActionEvent event){
+        modelo.comprarMejora(Model.Mejoras.HELADERA);
+    }
+    public void Caja_onClick(ActionEvent event){
+        modelo.comprarMejora(Model.Mejoras.CAJA_REGISTRADORA);
+    }
+    public void Empleados_onClick(ActionEvent event){
+        modelo.comprarMejora(Model.Mejoras.EMPLEADOS);
+    }
+    public void Sucursales_onClick(ActionEvent event){
+        modelo.comprarMejora(Model.Mejoras.SUCURSALES);
     }
 
+    //METODOS PRINCIPALES
+    public void Pancho_onClick(){
+        modelo.addPanchoClick(1);
+    }
 }
--- src/View/MainView.java
+++ src/View/MainView.java
@@ -1,19 +1,51 @@
 package src.View;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import src.Model.Observador;
 
-public class MainView extends Application  {
+public class MainView extends Application implements Observador {
     private FXMLLoader loader;
+
+    @FXML
+    private Button PanchoBtn;
+    @FXML
+    private Button CursorBtn;
+    @FXML
+    private Button CondimentosBtn;
+    @FXML
+    private Button PapasBtn;
+    @FXML
+    private Button BebidasBtn;
+    @FXML
+    private Button ParrillasBtn;
+    @FXML
+    private Button SalchicherasBtn;
+    @FXML
+    private Button HeladerasBtn;
+    @FXML
+    private Button CajaBtn;
+    @FXML
+    private Button EmpleadosBtn;
+    @FXML
+    private Button SucursalesBtn;
+    @FXML
+    private Label stockLbl;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+
         loader.setController(src.Main.controlador);
+
         Parent root = loader.load();
 
         Scene scene = new Scene(root);
@@ -32,4 +64,8 @@ public class MainView extends Application  {
         super.stop();
     }
 
+    @Override
+    public void update() {
+        stockLbl.setText();
+    }
 }
