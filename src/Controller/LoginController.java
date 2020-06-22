f25992cc55563ce4629551f3c110020a1ea790d8
Francisco De la Cruz <franciscoo.delacruzz@gmail.com>
2020-06-22 01:06:06 -0300

    Hola Panchowsa

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,48 @@
+package src.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import src.Main;
+import src.Model.Model;
+
+
+public class LoginController {
+    Model modelo;
+
+    @FXML
+    private TextField AliasTxt;
+    @FXML
+    private Button closeBtn;
+    public LoginController(Model mod){
+        modelo = mod;
+    }
+
+
+    public void Login_onClick(ActionEvent e){
+        if(AliasTxt.getText().isEmpty()){
+            Alert  alerta = new Alert(Alert.AlertType.INFORMATION,"Debes ingresar tu nombre", ButtonType.CLOSE);
+            alerta.setHeaderText(null);
+            alerta.setTitle("Alerta");
+            alerta.show();
+        }
+        else{
+            modelo.inicializarMejoras();
+            modelo.setAlias(AliasTxt.getText());
+            modelo.loadPlayerData();
+            Main.getMainView().getStage().show();
+            Stage stage = (Stage)AliasTxt.getScene().getWindow();
+            stage.close();
+        }
+
+    }
+    public void Close_onClick(ActionEvent e){
+        Stage stage = (Stage)closeBtn.getScene().getWindow();
+        stage.close();
+    }
+
+}
---
+++ src/View/LoginView.java
@@ -0,0 +1,44 @@
+package src.View;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import src.Controller.Controlador;
+import src.Controller.LoginController;
+import src.Model.Model;
+
+import java.io.IOException;
+
+public class LoginView {
+    Model modelo;
+    LoginController controlador;
+    FXMLLoader loader;
+    Scene loginScene;
+    Stage loginStage;
+
+    public LoginView(Model mod, LoginController cont) throws IOException {
+        modelo  = mod;
+        controlador = cont;
+
+        //Cargo ventana login
+        loader = new FXMLLoader(getClass().getResource("LoginView.fxml"));
+        loader.setController(controlador);
+        Parent login = loader.load();
+        loginScene = new Scene(login);
+        loginScene.getStylesheets().add(getClass().getResource("css/Texto.css").toExternalForm());
+        loginScene.getStylesheets().add(getClass().getResource("css/Login.css").toExternalForm());
+        loginStage = new Stage();
+        loginStage.setResizable(false);
+        //loginStage.setTitle("LePancherie - Login");
+        loginStage.setScene(loginScene);
+        loginStage.initStyle(StageStyle.UNDECORATED);
+        loginStage.initModality(Modality.APPLICATION_MODAL);
+    }
+
+    public Stage getStage(){
+        return loginStage;
+    }
+}
